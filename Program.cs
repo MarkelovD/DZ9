732f15cba99ed1532017ef3b3798ac3e2f0f991e
Dmitry <markelov16@gmail.com>
2022-11-14 20:02:55 +0500

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+Console.WriteLine("введите конечное число");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(n));
+string PrintNumbers(int end)
+{
+    if (end == 1) return end.ToString();
+    return (end + " " + PrintNumbers(end - 1));
+}
